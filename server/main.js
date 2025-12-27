8766f1b3653f14506b8b601c58ab9fb3b9e5713d
CaioEmPessoa <caiopolonioabruno@gmail.com>
2025-12-27 02:51:03 -0300

    Removed notes. Moved some things around. Started serving static files

--- server/main.js
+++ server/main.js
@@ -1,18 +1,25 @@
 const express = require('express');
-const path = require('path')
+const morgan = require('morgan')
+const { join } = require('path')
 
 const { debug } = require('./logs')
 
 const PORT = 2469;
-const ROOT = path.join(__dirname, "..");
+const ROOT = join(__dirname, "..");
 
-debug("ROOT PATH : " + ROOT);
+debug("Starting app...");
 
 const app = express();
-app.listen(PORT);
 
-debug(`RUNNING AT: http://localhost:${PORT}`)
+// Middleware 
+app.use(
+  express.static(join(ROOT, "public"))
+)
+app.use(
+  morgan('dev')
+);
 
+// Pages
 app.get('/', (req, res) => {
   res.sendFile('/public/index.html', { root: ROOT });
 });
@@ -22,12 +29,10 @@ app.use((req, res) => {
   res.status(404).send('<h1> Erro 404. </h1>', { root: ROOT });
 });
 
-/*
-  ANOTACOES: (REMOVER DEPOIS)
-    res.redirect -> redireciona par outra pagina
-    app.use -> redireciona qq url
+app.listen(PORT, () => {
+  
+  debug("Start finished!");
+  debug(`RUNNING AT: http://localhost:${PORT}`);
+  debug("ROOT PATH : " + ROOT);
 
-    Funcoes tem um 'return' no final. No caso o "use" do final
-    só roda caso nao encontre nenhuma rota seja executada anteriormente.
-
-*/
+});
