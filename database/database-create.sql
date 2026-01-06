4901ae4d4f4e0e36a9b790c9361c56f079437aca
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-01-06 19:49:51 -0300

    Changed database language

--- database/database-create.sql
+++ database/database-create.sql
@@ -4,57 +4,51 @@
 	USE HomeStock;
 */
 
-CREATE TABLE IF NOT EXISTS categorias (
-	categoria_id INT NOT NULL AUTO_INCREMENT,
-	categoria_nome VARCHAR(100),
-	categoria_imagem VARCHAR(300),
-	categoria_icone_texto CHAR(1),
-	PRIMARY KEY (categoria_id)
+CREATE TABLE IF NOT EXISTS categories (
+	category_id INT NOT NULL AUTO_INCREMENT,
+	category_name VARCHAR(100),
+	category_image VARCHAR(300),
+	category_icon_text CHAR(1),
+	PRIMARY KEY (category_id)
 );
 
-CREATE TABLE IF NOT EXISTS cargos(
-	cargo_id INT NOT NULL AUTO_INCREMENT,
-	cargo_nome VARCHAR(50),
-	cargo_perm TINYINT,
-	PRIMARY KEY (cargo_id)
+CREATE TABLE IF NOT EXISTS roles(
+	role_id INT NOT NULL AUTO_INCREMENT,
+	role_name VARCHAR(50),
+	role_permission TINYINT,
+	PRIMARY KEY (role_id)
 );
 
-CREATE TABLE IF NOT EXISTS usuarios (
-	usuario_id INT NOT NULL AUTO_INCREMENT,
-	usuario_nome VARCHAR(100),
-	usuario_email VARCHAR(100),
-	cargo_id INT,
-	PRIMARY KEY (usuario_id),
-	FOREIGN KEY (cargo_id) REFERENCES cargos(cargo_id)
+CREATE TABLE IF NOT EXISTS users (
+	user_id INT NOT NULL AUTO_INCREMENT,
+	user_name VARCHAR(100),
+	user_email VARCHAR(100),
+	role_id INT,
+	PRIMARY KEY (user_id),
+	FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
-CREATE TABLE IF NOT EXISTS produtos (
-	produto_id INT NOT NULL AUTO_INCREMENT,
-	produto_codBarras VARCHAR(20),
-	produto_imagem VARCHAR(30),
-	produto_icone CHAR(1),
-	produto_minimo TINYINT,
-	produto_maximo TINYINT,
-	categoria_id INT,
-	PRIMARY KEY (produto_id),
-	FOREIGN KEY (categoria_id) REFERENCES categorias(categoria_id)
+CREATE TABLE IF NOT EXISTS products (
+	product_id INT NOT NULL AUTO_INCREMENT,
+	product_barcode VARCHAR(20),
+	product_image VARCHAR(30),
+	product_icon CHAR(1),
+	product_minimum TINYINT,
+	product_maximum TINYINT,
+	category_id INT,
+	PRIMARY KEY (product_id),
+	FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
-CREATE TABLE IF NOT EXISTS produtos_estoque (
-	estoque_id INT NOT NULL AUTO_INCREMENT,
-	estoque_quantidade INT,
-	estoque_data_validade DATE,
-	estoque_data_fabricacao DATE,
-	produto_id INT,
-	produto_historico_usuario INT,
-	
-	PRIMARY KEY (estoque_id),
-	FOREIGN KEY (produto_id) REFERENCES produtos(produto_id),
-	FOREIGN KEY (produto_historico_usuario) REFERENCES usuarios(usuario_id)
-)
+CREATE TABLE IF NOT EXISTS inventory_items (
+	inventory_id INT NOT NULL AUTO_INCREMENT,
+	inventory_quantity INT,
+	inventory_expiry_date DATE,
+	inventory_manufacturing_date DATE,
+	product_id INT,
+	inventory_history_user INT,
 
-
-
-/*
-
-*/
+	PRIMARY KEY (inventory_id),
+	FOREIGN KEY (product_id) REFERENCES products(product_id),
+	FOREIGN KEY (inventory_history_user) REFERENCES users(user_id)
+);
