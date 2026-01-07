8eaad7ee6b8bef03598ca23b0476a9a40cb7f63c
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-01-07 16:30:43 -0300

    Fixed async issues, now register_index is working like a charm ;)

--- server/registers/registers_index.js
+++ server/registers/registers_index.js
@@ -1,76 +1,67 @@
 const mysql = require('mysql');
 const debug = require('../utilities/debug');
 
-const category_register = require('./category_register');
+const categoryRegister = require('./category_register');
+const rolesRegister    = require('./roles_register');
 
 class registersIndex {
 
-init() {
-    debug.log("Starting connection...");
+    async init() {
+        return new Promise((resolve, reject) => {
+        debug.log("Starting connection...");
 
-    this.connection = mysql.createConnection({
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT || 3306,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DATABASE
-    });
-
-    this.connection.connect((err) => {
-        if (err) {
-            debug.log(`Connection FAILED: ${err.code} - ${err.message}`, "ERROR");
-
-            if (err.code === 'ECONNREFUSED') {
-                debug.log("MariaDB server is not running or not accessible!", "ERROR");
-            } else if (err.code === 'ER_ACCESS_DENIED_ERROR') {
-                debug.log("Invalid username/password!", "ERROR");
-            } else if (err.code === 'ER_BAD_DB_ERROR') {
-                debug.log("Database doesn't exist!", "ERROR");
-            }
-        }
-        debug.log("Connection established successfully!");
-
-        // Now test with a query
-        this.testConnection().then(() => {
-            const boundQuerySQL = this.querySQL.bind(this);
-
-            debug.log("Start creating tables...")
-
-            category_register.init(boundQuerySQL);
-            category_register.createTable();
-
-            debug.log("Finished creating tables!")
+        this.connection = mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            port: process.env.DATABASE_PORT || 3306,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_DATABASE
         });
-    });
-}
 
-async testConnection() {
-    return new Promise((resolve, reject) => {
-        this.connection.query('SELECT 1+1 AS test', (error, results) => {
-            if (error) {
-                debug.log(`Connection failed: "${error.message}"`, "ERROR");
-                reject(error);
-            } else {
-                debug.log(`Queries working!`);
-                resolve(results);
+        this.connection.connect((err) => {
+            if (err) {
+                debug.log(`Connection FAILED: ${err.code} - ${err.message}`, "ERROR");
+
+                if (err.code === 'ECONNREFUSED') {
+                    debug.log("MariaDB server is not running or not accessible!", "ERROR");
+                } else if (err.code === 'ER_ACCESS_DENIED_ERROR') {
+                    debug.log("Invalid username/password!", "ERROR");
+                } else if (err.code === 'ER_BAD_DB_ERROR') {
+                    debug.log("Database doesn't exist!", "ERROR");
+                }
             }
+            debug.log("Connection established successfully!");
+
+            // Now test with a query
+            this.testConnection().then(() => {
+                // --------- START CREATING TABLES --------
+                debug.log("Start creating tables...");
+                const category_register = new categoryRegister(this.connection);
+                const roles_register = new rolesRegister(this.connection);
+
+                category_register.createTable();
+                roles_register.createTable();
+
+                debug.log("Finished creating tables!");
+
+                resolve(); // END OF REGISTERS INDEX INITIATION.
+            });
         });
-    });
-}
-
-querySQL(query, success_msg) {
-        this.connection.query(query, {}, (error, response) => {
-            if(error) {
-                debug.log("DATABASE QUERY ERROR: " + error, "ERROR");
-                debug.log("QEURY: " + query, "ERROR");
-                return error;
-            }
-            else {
-                debug.log(success_msg);
-                return response;
-            }
         })
+    }
 
+    async testConnection() {
+        return new Promise((resolve, reject) => {
+            this.connection.query('SELECT 1+1 AS test', (error, results) => {
+                if (error) {
+                    debug.log(`Connection failed: "${error.message}"`, "ERROR");
+                    reject(error);
+                } else {
+                    debug.log(`Queries working!`);
+                    resolve(results);
+                }
+            });
+        });
     }
 }
 
