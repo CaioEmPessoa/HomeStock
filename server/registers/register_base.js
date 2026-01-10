01e8dc9c6b1e154ca78263ed2b38fa9cd88cd934
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-01-09 23:03:08 -0300

    Added new "save" function into register_base

--- server/registers/register_base.js
+++ server/registers/register_base.js
@@ -3,18 +3,27 @@ const debug = require('../utilities/debug');
 class RegisterBase {
 
     constructor(connection, createTableQuery, tableName){
+        if(connection == null) {
+            debug.logError("Connection can't be null!");
+            throw new Error("RegisterBase Error: Connection can't be null!");
+        } else if (tableName == null) {
+            debug.logError("Please provide a table name!");
+            throw new Error("RegisterBase Error: Please provide a table name!");
+        }
+
         this.connection = connection;
 
         this.createTableQuery = createTableQuery;
         this.tableName = tableName;
+
     }
 
     querySQL(query, success_msg) {
         return new Promise((resolve, reject) => {
             this.connection.query(query, {}, (error, response) => {
                 if(error) {
-                    debug.log("DATABASE QUERY ERROR: " + error, "ERROR");
-                    debug.log("QUERY: " + query, "ERROR");
+                    debug.logError("DATABASE QUERY ERROR: " + error);
+                    debug.logError("QUERY: " + query);
                     reject(error);
                 }
                 else {
@@ -37,7 +46,25 @@ class RegisterBase {
             const response = await this.querySQL(sql, `Fetched all ${this.tableName}`);
             return response;
         } catch (error) {
-            debug.log(`Error fetching ${this.tableName}: ${error}`, "ERROR");
+            debug.logError(`Error fetching ${this.tableName}: ${error}`);
+            throw error;
+        }
+    }
+
+    /**
+     * Save values into current table.
+     * @param {[{columnName: "str", columnValue: "str"}]} entity - Entity data to save into this current table.
+    **/
+    async save(entity) {
+        let sql = `INSERT INTO ${this.tableName} (${entity.map(el => el.columnName)}) `;
+        sql    += `VALUES (${entity.map(el => ` '${el.columnValue}'`)} )`;
+
+        try {
+            const response = await this.querySQL(sql, `Saved into ${this.tableName}!`);
+            return response;
+        } catch (error) {
+            debug.logError(`Error saving into ${this.tableName}: ${error}`);
+            debug.logError(`SQL Query: ${sql}`);
             throw error;
         }
     }
