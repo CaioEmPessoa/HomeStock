cbb71fed752495e0ee989af5db6d78ffbabd7825
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-01-11 13:50:51 -0300

    Added new "save" method. Implemented ApiReturn class

--- server/services/category_service.js
+++ server/services/category_service.js
@@ -1,4 +1,5 @@
 const debug = require("../utilities/debug");
+const apiReturn = require("../utilities/api_return");
 
 class category_service {
 
@@ -11,13 +12,34 @@ class category_service {
         return new Promise(async (resolve, reject) => {
             try {
                 const result = await this.category_register.getAll();
-                resolve(result);
+                const api_return = apiReturn.success(null, "Categorias encontradas com sucesso!", result);
+
+                resolve(api_return);
             }
-            catch {
+            catch (error) {
                 debug.logError("Error on Category -> getAll()");
+                debug.logError(`ERROR: ${error}`);
+                reject(apiReturn.errorInternal());
             }
         })
 
+    }
+
+    async save(obj) {
+        debug.log("Category -> save()");
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await this.category_register.save(obj);
+                const api_return = apiReturn.success(null, "Successfully saved!", result);
+                resolve(api_return);
+            }
+            catch (error) {
+                debug.logError("Error on Category -> save()");
+                debug.logError(`ERROR: ${error}`);
+
+                reject(apiReturn.errorBadRequest(`Error on save! ${error.sqlMessage}`));
+            }
+        })
     }
 
 }
