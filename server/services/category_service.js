9cacec6078f5b7f0705cf125452bc92cea01b9d9
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-01-11 21:02:37 -0300

    Removed serivce methods from category and created a new base for them

--- server/services/category_service.js
+++ server/services/category_service.js
@@ -1,111 +1,15 @@
-const debug = require("../utilities/debug");
-const apiReturn = require("../utilities/api_return");
 
-class category_service {
+const servicesBase = require('./services_base');
+
+class categoryServie extends servicesBase{
 
     constructor (tables) {
-        this.category_register = tables.category_register;
-    }
+        const register = tables.category_register;
+        const tableName = "categories";
 
-    async getById(id) {
-        debug.log("Category -> getById()");
-        return new Promise(async (resolve, reject) => {
-            try {
-                const result = await this.category_register.getById(id);
-
-                if (result == false) {
-                    const api_return = apiReturn.success(null, "No category found!");
-                    return resolve(api_return);
-                }
-
-                const api_return = apiReturn.success(null, "Category found successfully!", result);
-                resolve(api_return);
-            }
-            catch (error) {
-                debug.logError("Error on Category -> getById()");
-                debug.logError(`ERROR: ${error}`);
-                reject(apiReturn.errorInternal());
-            }
-        })
-
-    }
-
-    async getAll() {
-        debug.log("Category -> getAll()");
-        return new Promise(async (resolve, reject) => {
-            try {
-                const result = await this.category_register.getAll();
-                const api_return = apiReturn.success(null, "Categorias encontradas com sucesso!", result);
-
-                resolve(api_return);
-            }
-            catch (error) {
-                debug.logError("Error on Category -> getAll()");
-                debug.logError(`ERROR: ${error}`);
-                reject(apiReturn.errorInternal());
-            }
-        })
-
-    }
-
-    async save(obj) {
-        debug.log("Category -> save()");
-        return new Promise(async (resolve, reject) => {
-            try {
-                const result = await this.category_register.save(obj);
-                const api_return = apiReturn.success(null, "Successfully saved!", result);
-                resolve(api_return);
-            }
-            catch (error) {
-                debug.logError("Error on Category -> save()");
-                debug.logError(`ERROR: ${error}`);
-
-                reject(apiReturn.errorBadRequest(`Error on save! ${error.sqlMessage}`));
-            }
-        })
-    }
-
-    async update(obj, id) {
-        debug.log("Category -> update()");
-        return new Promise(async (resolve, reject) => {
-            try {
-                const result = await this.category_register.update(obj, id);
-                const api_return = apiReturn.success(null, "Successfully saved!", result);
-                resolve(api_return);
-            }
-            catch (error) {
-                debug.logError("Error on Category -> update()");
-                debug.logError(`ERROR: ${error}`);
-
-                reject(apiReturn.errorBadRequest(`Error on update! ${error.sqlMessage}`));
-            }
-        })
-    }
-
-    async delete(id) {
-        debug.log("Category -> delete()");
-        return new Promise(async (resolve, reject) => {
-            try {
-                const search = await this.category_register.getById(id);
-                if (search == false) {
-                    const api_return = apiReturn.success(null, "No category found!");
-                    return resolve(api_return);
-                }
-
-                const result = await this.category_register.delete(id);
-
-                const api_return = apiReturn.success(null, "Successfully deleted!");
-                resolve(api_return);
-            }
-            catch (error) {
-                debug.logError("Error on Category -> delete()");
-                debug.logError(`ERROR: ${error}`);
-
-                reject(apiReturn.errorBadRequest(`Error on delete! ${error.sqlMessage}`));
-            }
-        })
+        super(register, tableName);
     }
 
 }
 
-module.exports = category_service;
+module.exports = categoryServie;
---
+++ server/services/services_base.js
@@ -0,0 +1,108 @@
+// base_service.js
+const debug = require("../utilities/debug");
+const apiReturn = require("../utilities/api_return");
+
+class servicesBase {
+    constructor(register, tableName) {
+        this.register = register;
+        this.tableName = tableName;
+    }
+
+    async getById(id) {
+        debug.log(`${this.tableName} -> getById()`);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await this.register.getById(id);
+
+                if (result == false) {
+                    const api_return = apiReturn.success(null, `No ${this.tableName.toLowerCase()} found!`);
+                    return resolve(api_return);
+                }
+
+                const api_return = apiReturn.success(null, `${this.tableName} found successfully!`, result);
+                resolve(api_return);
+            }
+            catch (error) {
+                debug.logError(`Error on ${this.tableName} -> getById()`);
+                debug.logError(`ERROR: ${error}`);
+                reject(apiReturn.errorInternal());
+            }
+        });
+    }
+
+    async getAll() {
+        debug.log(`${this.tableName} -> getAll()`);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await this.register.getAll();
+                const api_return = apiReturn.success(null, `${this.tableName}s found successfully!`, result);
+                resolve(api_return);
+            }
+            catch (error) {
+                debug.logError(`Error on ${this.tableName} -> getAll()`);
+                debug.logError(`ERROR: ${error}`);
+                reject(apiReturn.errorInternal());
+            }
+        });
+    }
+
+    async save(obj) {
+        debug.log(`${this.tableName} -> save()`);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await this.register.save(obj);
+                const api_return = apiReturn.success(null, "Successfully saved!", result);
+                resolve(api_return);
+            }
+            catch (error) {
+                debug.logError(`Error on ${this.tableName} -> save()`);
+                debug.logError(`ERROR: ${error}`);
+
+                reject(apiReturn.errorBadRequest(`Error on save! ${error.sqlMessage}`));
+            }
+        });
+    }
+
+    async update(obj, id) {
+        debug.log(`${this.tableName} -> update()`);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const result = await this.register.update(obj, id);
+                const api_return = apiReturn.success(null, "Successfully updated!", result);
+                resolve(api_return);
+            }
+            catch (error) {
+                debug.logError(`Error on ${this.tableName} -> update()`);
+                debug.logError(`ERROR: ${error}`);
+
+                reject(apiReturn.errorBadRequest(`Error on update! ${error.sqlMessage}`));
+            }
+        });
+    }
+
+    async delete(id) {
+        debug.log(`${this.tableName} -> delete()`);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const search = await this.register.getById(id);
+                if (search == false) {
+                    const api_return = apiReturn.success(null, `No ${this.tableName.toLowerCase()} found!`);
+                    return resolve(api_return);
+                }
+
+                const result = await this.register.delete(id);
+                const api_return = apiReturn.success(null, "Successfully deleted!");
+                resolve(api_return);
+            }
+            catch (error) {
+                debug.logError(`Error on ${this.tableName} -> delete()`);
+                debug.logError(`ERROR: ${error}`);
+
+                reject(apiReturn.errorBadRequest(`Error on delete! ${error.sqlMessage}`));
+            }
+        });
+    }
+
+}
+
+module.exports = servicesBase;
