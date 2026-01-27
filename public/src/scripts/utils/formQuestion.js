fc970890d5774ebca01afd27af15815350693b9c
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-01-27 09:40:58 -0300

    Updated documentation for formQuestion

--- public/src/scripts/utils/formQuestion.js
+++ public/src/scripts/utils/formQuestion.js
@@ -1,11 +1,16 @@
 
 class FormQuestion {
 
+    /**
+     * Generates a FormQuestion of choice. Used inside a modal.
+     * @param {string} title -> (Optional) Title for the generated formQuestion
+     * @param {*} customClass -> (Optional) Custom class for the generated FormQuestion
+     */
     constructor (title, customClass) {
         this.title = title || "";
         this.customClass = customClass || "";
     }
-    
+
     _genModalFormQuestion() {
         let modalFormQuestion = document.createElement('div');
         // Always adds the class "modal-form-question"
@@ -23,6 +28,12 @@ class FormQuestion {
         return modalFormQuestion;
     }
 
+    /**
+     *  Generate a generic HTML input.
+     * @param {string} inputType -> HTML Input type.
+     * @param {string} name -> Form name
+     * @returns
+     */
     generic(inputType="", name="") {
 
         let modalFormQuestion = this._genModalFormQuestion();
@@ -36,13 +47,29 @@ class FormQuestion {
 
         return modalFormQuestion;
     }
-
+    /**
+     * Generates a text input form
+     * @param {string} name Form name
+     * @returns
+     */
     text(name="") {
         return this.generic("text", name);
     }
+    /**
+     * Generates a date input form
+     * @param {string} name Form name
+     * @returns
+     */
     date(name="") {
         return this.generic("date", name);
     }
+    /**
+     * Generates a number input form
+     * @param {string} name Form name
+     * @param {min} name -> (Optional) Minimum value for num
+     * @param {max} name -> (Optional) Minimum value for num
+     * @returns
+     */
     number(name="", min, max) {
         let modalFormQuestion = this.generic("number", name);
 
@@ -57,7 +84,12 @@ class FormQuestion {
     paragraph() {
         /* TODO */
     }
-
+    /**
+     * Returns a radio input element
+     * @param {{optionName: optionValue, optionName: optionName}} options
+     * @param {*} name
+     * @returns
+     */
     select(options = [], name="") {
         let modalFormQuestion = this._genModalFormQuestion();
 
@@ -85,7 +117,13 @@ class FormQuestion {
         return modalFormQuestion;
 
     }
-
+    /**
+     * Returns a dropdown HTML element.
+     * @param {[{value: string, name: string}]} options
+     * @param {string} name -> Form name
+     * @param {string} defaultOption -> Default option to be selected as "null"
+     * @returns
+     */
     dropdown(options = [], name="", defaultOption="") {
         let modalFormQuestion = this._genModalFormQuestion();
 
@@ -107,6 +145,12 @@ class FormQuestion {
         return modalFormQuestion;
     }
 
+    /**
+     * Generates a toggle (checkbox) input.
+     * @param {string} label -> Checkbox label to appear side-by-side
+     * @param {*} name -> Form name
+     * @returns
+     */
     toggle(label="", name="") {
         let modalFormQuestion = this._genModalFormQuestion();
 
@@ -120,7 +164,7 @@ class FormQuestion {
         let modalToggleInput = document.createElement("input");
         modalToggleInput.type = "checkbox";
         modalToggleInput.name = name;
-        
+
         let modalToggleSpan = document.createElement("span");
         modalToggleSpan.classList.add("modal-form-toggle-slider");
 
@@ -140,11 +184,16 @@ class FormQuestion {
     }
 
     // Other components
+    /**
+     *
+     * @param {boolean} showHr -> Show or not show a divisor bar (hr) above the title
+     * @returns
+     */
     spacer(showHr=true) {
         let modalFormQuestion = document.createElement('div');
         modalFormQuestion.classList.add("modal-form-question");
 
-        
+
         let modalQuestionTitle = document.createElement('h3');
         modalQuestionTitle.innerText = this.title;
 
@@ -158,8 +207,11 @@ class FormQuestion {
         return modalFormQuestion;
     }
 
-    // Display a list of other components
-    // next to each other on the same row.
+    /**
+     * Display a list of components next to each other on the same row.
+     * @param {*} components -> List of FormQuestion components
+     * @returns
+     */
     double(components=[]) {
         let modalFormQuestion = document.createElement('div');
         modalFormQuestion.classList.add("modal-form-question");
